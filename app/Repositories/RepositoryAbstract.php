f33528975aedd27758df84ad6a0c00ca88e9784d
IT <italo@kampa.io>
2023-11-14 12:33:08 -0300

    creating abstract repository class

---
+++ app/Repositories/RepositoryAbstract.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Connection\DatabaseConnection;
+use PDO;
+
+class RepositoryAbstract
+{
+  private PDO $pdo;
+
+  public function __construct()
+  {
+    $this->pdo = DatabaseConnection::openConnection();
+  }
+
+  public function save(string $table, string $columns, array $values): void
+  {
+
+    $query = "INSERT INTO " . $table . "($columns) VALUES('" . implode("', '", $values) . "')";
+
+    $this->pdo->query($query);
+  }
+
+  public function update(string $table, array $data, int $id): void
+  {
+    $formattedData = array_map(
+      function ($key, $value) {
+        return "$key = '$value'";
+      },
+      array_keys($data),
+      $data
+    );
+
+    $query = "UPDATE " . $table . " SET '" . implode(', ', $formattedData) . "' WHERE id = " . $id;
+
+    $this->pdo->query($query);
+  }
+
+  public function delete(string $table, int $id, bool $softDelete): void
+  {
+    $query = "DELETE FROM " . $table . " WHERE id = " . $id;
+
+    if ($softDelete === true) {
+      $query = "UPDATE " . $table . " SET deleted_at = '" . date('Y/m/d h:i:s'). "' WHERE id = " . $id;
+    }
+
+    $this->pdo->query($query);
+  }
+}
